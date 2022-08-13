20e42160d2c9f891a16aa1c3116ff85a94b9ac0b
Aknexad <aknejad@outlook.com>
2022-08-13 12:50:57 +0430

    add handel click and render board

--- script.js
+++ script.js
@@ -1,22 +1,83 @@
-const selectType = document.querySelectorAll(
-  '[data-select-type]'
-);
+const GameBoard = {
+  board: ['', '', '', '', '', '', '', '', ''],
+  player: {
+    class: 'x',
+    score: 0,
+  },
+  computer: {
+    class: 'o',
+    score: 0,
+  },
+};
 
-let playerClassSelect = 'X';
+const cell = document.querySelectorAll('[data-cell]');
 
-selectType.forEach((element) => {
-  element.addEventListener('click', (e) => {
-    playerClassSelect = e.target.innerText;
-    if (playerClassSelect === 'O') {
-      document
-        .querySelector('.btn-select')
-        .classList.remove('btn-select');
-      element.classList.add('btn-select');
-    } else {
-      document
-        .querySelector('.btn-select')
-        .classList.remove('btn-select');
-      element.classList.add('btn-select');
-    }
+cell.forEach((element) => {
+  element.addEventListener('click', handelCellClick, {
+    once: true,
   });
 });
+
+function handelCellClick(e) {
+  const idCell = e.target.id;
+  const board = GameBoard.board;
+  board[idCell] = 'X';
+  const emty = board.filter((word) => word == '');
+  renderBoard();
+}
+
+function renderBoard() {
+  for (let i = 0; i < cell.length; i++) {
+    const element = cell[i];
+    element.innerText = GameBoard.board[i];
+  }
+}
+
+// const selectclass = document.querySelectorAll(
+//   '[data-select-class]'
+// );
+// const cell = document.querySelectorAll('[ data-cell]');
+
+// selectclass.forEach((element) => {
+//   element.addEventListener('click', handelClassSelect);
+// });
+// cell.forEach((element) => {
+//   element.addEventListener('click', handeClickCell, {
+//     once: true,
+//   });
+// });
+
+// function handelClassSelect(e) {
+//   if (e.target.innerText === 'O') {
+//     document
+//       .querySelector('.btn-select')
+//       .classList.remove('btn-select');
+//     e.target.classList.add('btn-select');
+//     GameBoard.player.class = 'O';
+//     GameBoard.pc.class = 'X';
+//   } else {
+//     document
+//       .querySelector('.btn-select')
+//       .classList.remove('btn-select');
+//     e.target.classList.add('btn-select');
+//   }
+// }
+
+// function reanderBoard() {
+//   let bord = GameBoard.board;
+//   let cell = document.querySelectorAll('[data-cell]');
+//   for (let i = 0; i < cell.length; i++) {
+//     const element = cell[i];
+//     element.classList.add(bord[i]);
+//   }
+// }
+
+// function handeClickCell(e) {
+//   const cell = e.target;
+//   placeMark(cell, GameBoard.player.class);
+// }
+
+// function placeMark(cell, playerClass) {
+//   cell.classList.add(playerClass);
+
+// }
