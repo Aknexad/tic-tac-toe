3b086321af04e6d91eb7f597f2543d6aa6b7bd3f
Aknexad <aknejad@outlook.com>
2022-08-17 12:36:19 +0430

    add computer function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const GameBoard = {
   board: ['', '', '', '', '', '', '', '', ''],
+  indexs: [],
+  winningCombinations: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 6, 4],
+  ],
   player: {
     class: 'x',
     score: 0,
@@ -22,7 +33,8 @@ function handelCellClick(e) {
   const idCell = e.target.id;
   const board = GameBoard.board;
   board[idCell] = 'X';
-  const emty = board.filter((word) => word == '');
+  computer();
+
   renderBoard();
 }
 
@@ -33,51 +45,24 @@ function renderBoard() {
   }
 }
 
-// const selectclass = document.querySelectorAll(
-//   '[data-select-class]'
-// );
-// const cell = document.querySelectorAll('[ data-cell]');
+function computer() {
+  findeEmptyIndex();
+  let emptyIndex = GameBoard.indexs;
+  let board = GameBoard.board;
+  let randumNumber = Math.floor(
+    Math.random() * emptyIndex.length
+  );
+  GameBoard.board[emptyIndex[randumNumber]] = 'O';
+}
 
-// selectclass.forEach((element) => {
-//   element.addEventListener('click', handelClassSelect);
-// });
-// cell.forEach((element) => {
-//   element.addEventListener('click', handeClickCell, {
-//     once: true,
-//   });
-// });
-
-// function handelClassSelect(e) {
-//   if (e.target.innerText === 'O') {
-//     document
-//       .querySelector('.btn-select')
-//       .classList.remove('btn-select');
-//     e.target.classList.add('btn-select');
-//     GameBoard.player.class = 'O';
-//     GameBoard.pc.class = 'X';
-//   } else {
-//     document
-//       .querySelector('.btn-select')
-//       .classList.remove('btn-select');
-//     e.target.classList.add('btn-select');
-//   }
-// }
-
-// function reanderBoard() {
-//   let bord = GameBoard.board;
-//   let cell = document.querySelectorAll('[data-cell]');
-//   for (let i = 0; i < cell.length; i++) {
-//     const element = cell[i];
-//     element.classList.add(bord[i]);
-//   }
-// }
-
-// function handeClickCell(e) {
-//   const cell = e.target;
-//   placeMark(cell, GameBoard.player.class);
-// }
-
-// function placeMark(cell, playerClass) {
-//   cell.classList.add(playerClass);
-
-// }
+function findeEmptyIndex() {
+  GameBoard.indexs = [];
+  let indexs = GameBoard.indexs;
+  let board = GameBoard.board;
+  for (let i = 0; i < board.length; i++) {
+    const element = board[i];
+    if (element === '') {
+      indexs.push(i);
+    }
+  }
+}
