0c1f360c533a8725f678ed38b1fbc9e06b1ba8c9
Aknexad <aknejad@outlook.com>
2022-08-19 11:54:23 +0430

    add remove event listener

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const GameBoard = {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 6, 4],
+    [2, 4, 6],
   ],
   player: {
     class: 'x',
@@ -19,20 +19,57 @@ const GameBoard = {
     class: 'o',
     score: 0,
   },
+  DomFunction: {
+    winningMassage: (win) => {
+      const winmassage =
+        document.querySelector('#winningText');
+      winmassage.innerText = `${win} is winner`;
+      document
+        .querySelector('#winningMassge')
+        .classList.add('show');
+    },
+    timeoutForMassage: () => {
+      setTimeout(() => {
+        document
+          .querySelector('#winningMassge')
+          .classList.remove('show');
+      }, 1000);
+    },
+    removeEventListener: () => {
+      cell.forEach((element) => {
+        element.removeEventListener(
+          'click',
+          handelCellClick
+        );
+        element.addEventListener('click', handelCellClick, {
+          once: true,
+        });
+      });
+    },
+  },
 };
 
 const cell = document.querySelectorAll('[data-cell]');
+const resatBtn = document.querySelector('#restart');
 
 cell.forEach((element) => {
   element.addEventListener('click', handelCellClick, {
     once: true,
   });
 });
+resatBtn.addEventListener('click', handelClickReaset);
+
+function handelClickReaset(e) {
+  GameBoard.player.score = 0;
+  GameBoard.computer.score = 0;
+  document.querySelector('#x-score').innerText = 0;
+  document.querySelector('#o-score').innerText = 0;
+  clearBoard();
+  GameBoard.DomFunction.removeEventListener();
+}
 
 function handelCellClick(e) {
-  const idCell = e.target.id;
-  const board = GameBoard.board;
-  board[idCell] = 'X';
+  player(e.target.id);
   computer();
   renderBoard();
   chackForWin();
@@ -45,13 +82,20 @@ function renderBoard() {
   }
 }
 
+function player(id) {
+  const board = GameBoard.board;
+  if (board[id] === '') {
+    board[id] = 'X';
+  }
+}
+
 function computer() {
   findeEmptyIndex();
   let emptyIndex = GameBoard.indexs;
-  let board = GameBoard.board;
   let randumNumber = Math.floor(
     Math.random() * emptyIndex.length
   );
+
   GameBoard.board[emptyIndex[randumNumber]] = 'O';
 }
 
@@ -69,7 +113,7 @@ function findeEmptyIndex() {
 
 function chackForWin() {
   let win;
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i <= 7; i++) {
     const winCondition = GameBoard.winningCombinations[i];
     let a = GameBoard.board[winCondition[0]];
     let b = GameBoard.board[winCondition[1]];
@@ -95,12 +139,41 @@ function chackForWin() {
       c === 'O'
     ) {
       win = 'o';
+      break;
     }
   }
   if (win == 'x') {
-    console.log(`${win} is winner`);
+    GameBoard.DomFunction.winningMassage(win);
+    GameBoard.DomFunction.timeoutForMassage();
+    GameBoard.player.score++;
+    scoreBoard(win);
+    clearBoard();
+    GameBoard.DomFunction.removeEventListener();
   }
   if (win === 'o') {
-    console.log(`${win} is winner`);
+    GameBoard.DomFunction.winningMassage(win);
+    GameBoard.DomFunction.timeoutForMassage();
+    GameBoard.computer.score++;
+    scoreBoard(win);
+    clearBoard();
+    GameBoard.DomFunction.removeEventListener();
   }
+}
+
+function scoreBoard(win) {
+  const xScoreId = document.querySelector('#x-score');
+  const oScoreId = document.querySelector('#o-score');
+  if (win === 'x') {
+    xScoreId.innerText = GameBoard.player.score;
+  } else {
+    oScoreId.innerText = GameBoard.computer.score;
+  }
+}
+
+function clearBoard() {
+  let board = GameBoard.board;
+  for (let i = 0; i < board.length; i++) {
+    board[i] = '';
+  }
+  renderBoard();
 }
